944dfa6d877fc3a207430d2b92a83ddbeeb6f3e1
Shahidul1786 <stshahidislam1786@gmail.com>
2019-09-19 22:21:54 +0600

    complete up to 2

---
+++ SpringMvc1/src/main/resources/com/shahid/AddController.java
@@ -0,0 +1,15 @@
+package com.shahid;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AddController {
+	
+	@RequestMapping("/add")
+	public String add() {
+		
+		return "display.jsp";
+	}
+
+}
