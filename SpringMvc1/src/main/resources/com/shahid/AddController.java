fff8254569d76a7de6fb1d2b06cb6ae0bfb028fc
Shahidul1786 <stshahidislam1786@gmail.com>
2019-09-22 23:35:48 +0600

    complete springMvc1 but error

--- SpringMvc1/src/main/resources/com/shahid/AddController.java
+++ SpringMvc1/src/main/resources/com/shahid/AddController.java
@@ -1,32 +1,26 @@
 package com.shahid;
 
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AddController {
-	
-	/*@RequestMapping("/add")
+
+	@RequestMapping("add")
 	public ModelAndView add(HttpServletRequest request,HttpServletResponse response) {
+		
 		int i = Integer.parseInt(request.getParameter("t1"));
 		int j = Integer.parseInt(request.getParameter("t2"));
 		
-		int k = i +j;
+		int k = i+j;
 		
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("display.jsp");
-		mv.addObject("result", k);
-		
+		mv.setViewName("dispaly.jsp");
+		mv.addObject("result", k)
 		
 		return mv;
-	}
-*/
-	
-	@RequestMapping("/add")
-	public void add() {
-		
-		System.out.println("I'm here");
 	}
 }
--- SpringMvc1/src/main/webapp/display.jsp
+++ SpringMvc1/src/main/webapp/display.jsp
@@ -1,12 +1,13 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core_rt" %>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta http-equiv="contentType" content="text/html; charset=ISO-8859-1">
 <title>Insert title here</title>
 </head>
 <body>
-	Result is:<%= request.getAttribute("result") %>
+ 	Result is : <%= request.getAttribute("result") %>
 </body>
 </html>
